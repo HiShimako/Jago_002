843029f447346a361dba755041e91099582f4950
Nakaishi <esmt.n2006@gmail.com>
2023-09-11 14:38:17 +0900

    Initial Commit

---
+++ Jago_002/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jago_002
+//
+//  Created by user on 2023/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
