d0baf793dc232d11a56aedec963c5135e4df0718
Nakaishi <esmt.n2006@gmail.com>
2023-09-11 15:03:33 +0900

    FileとStoryBoad繋げられず

---
+++ Jago_002/MainTableView.swift
@@ -0,0 +1,8 @@
+//
+//  MainTableView.swift
+//  Jago_002
+//
+//  Created by user on 2023/09/11.
+//
+
+import Foundation
---
+++ Jago_002/RecordedTextView.swift
@@ -0,0 +1,8 @@
+//
+//  RecordedTextView.swift
+//  Jago_002
+//
+//  Created by user on 2023/09/11.
+//
+
+import Foundation
