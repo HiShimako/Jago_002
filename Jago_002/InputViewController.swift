546d913deaf54306408f6181e39212ad5dd9e8eb
Nakaishi <esmt.n2006@gmail.com>
2023-09-22 13:20:02 +0900

    一旦保存inputViewを整理

--- Jago_002/InputViewController.swift
+++ Jago_002/InputViewController.swift
@@ -4,80 +4,67 @@
 //
 //  Created by user on 2023/09/12.
 //
-
 import UIKit
 
 class InputViewController: UIViewController {
     
-    
+    // MARK: - Properties
     var personName: String?
     var smallImage: UIImage?
     var bigImage: UIImage?
     
-    
+    // MARK: - Outlets
     @IBOutlet weak var personNameTextField: UITextField!
     @IBOutlet weak var personsSmallPhotoImageView: UIImageView!
     @IBOutlet weak var personsBigPhotoImageView: UIImageView!
     
-    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        personsSmallPhotoImageView.image = smallImage!
-        personsBigPhotoImageView.image = bigImage!
-        
+        if let smallImage = smallImage {
+            personsSmallPhotoImageView.image = smallImage
+        }
+        if let bigImage = bigImage {
+            personsBigPhotoImageView.image = bigImage
+        }
     }
     
+    // MARK: - Actions
     @IBAction func postAction(_ sender: Any) {
-        guard let personDict = createPersonDict() else {
-            return
-        }
+        guard let personDict = createPersonDict() else { return }
         
         var personsArray = fetchPersonsArray()
         personsArray.append(personDict)
         
         savePersonsArray(personsArray)
         
-        self.navigationController?.popViewController(animated: true)
+        navigationController?.popViewController(animated: true)
     }
-
-    // ユーザー入力からデータを取得する関数
-    func createPersonDict() -> [String: Any]? {
+    
+    // MARK: - Helper Functions
+    private func createPersonDict() -> [String: Any]? {
         guard let personName = personNameTextField.text,
               let smallImageData = personsSmallPhotoImageView.image?.jpegData(compressionQuality: 0.01),
               let bigImageData = personsBigPhotoImageView.image?.jpegData(compressionQuality: 0.01) else {
             return nil
         }
         
-        var personDict: [String: Any] = [
+        return [
             "personName": personName,
             "smallImage": smallImageData,
             "bigImage": bigImageData,
             "comments": []
         ]
-        
-        return personDict
     }
-
-    // UserDefaultsからpersonsArrayを取得する関数
-    func fetchPersonsArray() -> [[String: Any]] {
+    
+    private func fetchPersonsArray() -> [[String: Any]] {
         if let savedPersonsArray = UserDefaults.standard.array(forKey: "personsArray") as? [[String: Any]] {
             return savedPersonsArray
-        } else {
-            return []
         }
+        return []
     }
-
-    // personsArrayに新しいデータを追加し、再び保存する関数
-    func savePersonsArray(_ personsArray: [[String: Any]]) {
+    
+    private func savePersonsArray(_ personsArray: [[String: Any]]) {
         UserDefaults.standard.setValue(personsArray, forKey: "personsArray")
     }
-
-
-    
-    
-    
 }
-
-
