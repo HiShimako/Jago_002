155e7851f7958aa1f86f4bdf7c004d13d52ed581
Nakaishi <esmt.n2006@gmail.com>
2023-09-22 10:30:36 +0900

    背景色変えられた。セグメントボタン表示が反映されず。

--- Jago_002/RecordingViewController.swift
+++ Jago_002/RecordingViewController.swift
@@ -12,89 +12,101 @@ class RecordingViewController: UIViewController {
     @IBOutlet weak var recordingView: UIImageView!
     @IBOutlet weak var backGroundView: UIImageView!
     @IBOutlet weak var animationSetSelector: UISegmentedControl!
-
+    
     var personsArray: [[String: Any]]!
     var receivedRow: Int?
     var backGroundImageArray: [UIImage] = []
     var selectedSegment: Int = 0
     var comments: [[String: Any]] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let image = UIImage(named: "2_out001") {
-            animationSetSelector.setImage(image, forSegmentAt: 0)
+        print("Loaded \(backGroundImageArray.count) background images.")
+        
+        if let animationSet = AnimationSet(rawValue: AnimationSet.setOne.rawValue) {
+            loadAnimationImages(for: animationSet)
         }
-
+        
         personsArray = UserDefaults.standard.array(forKey: "personsArray") as? [[String: Any]] ?? []
         audioEngine = AVAudioEngine()
-
+        
         // Initialize segments
         animationSetSelector.setTitle(" ", forSegmentAt: 0)
         animationSetSelector.setTitle(" ", forSegmentAt: 1)
         animationSetSelector.setImage(AnimationSet.setOne.firstImage, forSegmentAt: 0)
         animationSetSelector.setImage(AnimationSet.setTwo.firstImage, forSegmentAt: 1)
- 
-
+        
+        
         if let segmentTitle = animationSetSelector.titleForSegment(at: animationSetSelector.selectedSegmentIndex),
            let animationSet = AnimationSet(rawValue: segmentTitle) {
             loadAnimationImages(for: animationSet)
         }
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         guard let receivedRow = receivedRow, receivedRow < personsArray.count,
-                let _ = personsArray[receivedRow]["bigImage"] as? Data else {
-              return
-          }
+              let _ = personsArray[receivedRow]["bigImage"] as? Data else {
+            return
+        }
         
-
+        
         
         recordingView.image = UIImage(data: personsArray[receivedRow]["bigImage"] as! Data)
-
+        
         try? startLiveTranscription()
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         SFSpeechRecognizer.requestAuthorization { _ in }
     }
-
+    
     enum AnimationSet: String {
         case setOne = "2_out00"
         case setTwo = "4_out00"
-
+        
         var firstImage: UIImage? {
             return UIImage(named: "\(self.rawValue)1")
         }
     }
-
+    
     @IBAction func animationSetChanged(_ sender: UISegmentedControl) {
-        guard let segmentTitle = sender.titleForSegment(at: sender.selectedSegmentIndex),
-              let animationSet = AnimationSet(rawValue: segmentTitle) else {
+        var animationSet: AnimationSet?
+        switch sender.selectedSegmentIndex {
+        case 0:
+            animationSet = .setOne
+        case 1:
+            animationSet = .setTwo
+        default:
             return
         }
-        loadAnimationImages(for: animationSet)
+        
+        if let animationSet = animationSet {
+            loadAnimationImages(for: animationSet)
+        }
     }
-
+    
     func loadAnimationImages(for set: AnimationSet) {
         backGroundImageArray = []
         while let backgroundImage = UIImage(named: "\(set.rawValue)\(backGroundImageArray.count+1)") {
             backGroundImageArray.append(backgroundImage)
         }
         backGroundView.animationImages = backGroundImageArray
+        backGroundView.animationDuration = 1.0 // 1秒
+        backGroundView.animationRepeatCount = 0 // 無限
         backGroundView.startAnimating()
     }
-
+    
     func stopLiveTranscription() {
         audioEngine.stop()
         audioEngine.inputNode.removeTap(onBus: 0)
         recognitionReq?.endAudio()
     }
-
+    
     func startLiveTranscription() throws {
         if let recognitionTask = self.recognitionTask {
             recognitionTask.cancel()
@@ -105,11 +117,11 @@ class RecordingViewController: UIViewController {
         recognitionReq = SFSpeechAudioBufferRecognitionRequest()
         guard let recognitionReq = recognitionReq else { return }
         recognitionReq.shouldReportPartialResults = true
-
+        
         let audioSession = AVAudioSession.sharedInstance()
         try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
         try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
-
+        
         let inputNode = audioEngine.inputNode
         let recordingFormat = inputNode.outputFormat(forBus: 0)
         inputNode.installTap(onBus: 0, bufferSize: 2048, format: recordingFormat) { (buffer, _) in
@@ -124,7 +136,7 @@ class RecordingViewController: UIViewController {
             }
         }
     }
-
+    
     @IBAction func stopRecording(_ sender: Any) {
         stopLiveTranscription()
         // receivedRowを安全にアンラップ
@@ -135,15 +147,15 @@ class RecordingViewController: UIViewController {
             UserDefaults.standard.set(personsArray, forKey: "personsArray")
             self.navigationController?.popViewController(animated: true)
         }
-
+        
     }
-
+    
     func createCommentDict() -> [String: Any] {
         let currentDate = Date()
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy/MM/dd HH:mm"
         let currentDateString = formatter.string(from: currentDate)
-
+        
         return [
             "time": currentDateString,
             "comment": textView.text ?? ""
